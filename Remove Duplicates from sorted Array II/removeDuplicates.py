4827b82d4efbf2d7ce5300b9ca0348292814c4f7
Bimarsh-Khatri12 <bimarshkhatri123@gmail.com>
2025-02-04 21:05:33 +0545

    Leetcode series

--- Remove Duplicates from sorted Array II/removeDuplicates.py
+++ Remove Duplicates from sorted Array II/removeDuplicates.py
@@ -4,7 +4,6 @@ def removeDuplicates(nums):
         if nums[i] != nums[k-2]:
             nums[k] = nums[i]
             k = k + 1
-    print(nums)
     return k
 
 print(removeDuplicates([0,0,1,1,1,1,2,3,3]))
